cbbd7fca22510e8fa3013e59a20bd0c59d52a660
marvinle <1634771973@qq.com>
2018-12-27 20:48:21 +0800

    add Cmakelist.txt to build

---
+++ src/build/CMakeFiles/lcjson.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lcjson.dir/test.c.o"
+  "CMakeFiles/lcjson.dir/lcjson.c.o"
+  "lcjson.pdb"
+  "lcjson"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lcjson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
